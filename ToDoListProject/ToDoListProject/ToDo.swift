85ad5ebcb9c278c7e08dbdc11c38c72668fc3b3c
bkilambi <45800657+bkilambi@users.noreply.github.com>
2020-08-11 07:26:07 -0700

    Completed Iteration 0

---
+++ ToDoListProject/ToDoListProject/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  ToDoListProject
+//
+//  Created by Apple on 8/11/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+class ToDoClass {
+
+   var description = ""
+   var important = false
+
+}
